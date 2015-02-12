b6609d97fb5a1885307242ef8a69bc24e91328fe
Marcus Schäfer <ms@suse.de>
2015-02-12 17:52:40 +0100

    Added support for internal/external ip discovery
    In addition refactor the code to be loosely coupled. Use
    extra modules for disk tag and agent metadata discovery.

---
+++ azuremetadata/usr/sbin/AzureAgent.pm
@@ -0,0 +1,84 @@
+#Copyright (C) 2015 SUSE LLC
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package AzureAgent;
+
+use strict;
+use XML::LibXML;
+use FileHandle;
+
+our @ISA    = qw (Exporter);
+our @EXPORT_OK = qw (
+    import_config
+    read_internal_ip
+    read_external_ip
+);
+
+sub import_config {
+    my $config_file = shift;
+    if (! $config_file) {
+        $config_file = '/var/lib/waagent/SharedConfig.xml';
+    }
+    my $handle = FileHandle->new();
+    if (! $handle->open($config_file)) {
+        die "Can't open file $config_file: $!";
+    }
+    binmode $handle;
+    my $libxml = XML::LibXML->new();
+    return $libxml->parse_fh($handle);
+}
+
+sub read_internal_ip {
+    my $xml = shift;
+    my $metadata = __read_metadata_node($xml);
+    if (! $metadata) {
+        return;
+    }
+    return $metadata->getAttribute('address');
+}
+
+sub read_external_ip {
+    my $xml = shift;
+    my $metadata = __read_metadata_node($xml);
+    my $endpoints = $metadata->getElementsByTagName('Endpoint');
+    my $address;
+    for (my $i=1;$i<= $endpoints->size();$i++) {
+        my $endpoint = $endpoints->get_node($i);
+        my $name = $endpoint->getAttribute('name');
+        if ($name eq 'SSH') {
+            $address = $endpoint->getAttribute('loadBalancedPublicAddress');
+            if ($address =~ /(.*):.*/) {
+                $address = $1;
+            }
+            return $address;
+        }
+    }
+    return $address;
+}
+
+sub __read_metadata_node {
+    my $xml = shift;
+    my $instances = $xml->getElementsByTagName('Instance');
+    for (my $i=1;$i<= $instances->size();$i++) {
+        my $instance = $instances->get_node($i);
+        my $id = $instance->getAttribute('id');
+        if ($id eq 'metadata') {
+            return $instance;
+        }
+    }
+    return;
+}
+
+1;
