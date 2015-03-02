0fb30c2376dc196b5bf7edd2105a11a737315bd5
Robert Schweikert <rjschwei@suse.com>
2015-03-02 11:43:50 -0500

    - ec2utils -> ec2deprecateimg   + Preamble formatting

--- ec2utils/ec2deprecateimg/setup.py
+++ ec2utils/ec2deprecateimg/setup.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2015 SUSE LLC, Robert Schweikert <rjschwei@suse.com>
 #
 # This file is part of ec2deprecateimg.
+#
 # ec2deprecateimg is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
