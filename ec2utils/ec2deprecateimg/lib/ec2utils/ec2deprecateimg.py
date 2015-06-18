22190ed95aeb34c60445053a34851506f96bf469
Robert Schweikert <rjschwei@suse.com>
2015-06-18 19:01:29 -0400

    Fix issue #22 Fix issue #23 - ec2utils - ec2deprecate image   + Version 2.0   + Integrate with the ec2utils base class   + Fix use of --version

--- ec2utils/ec2deprecateimg/lib/ec2utils/ec2deprecateimg.py
+++ ec2utils/ec2deprecateimg/lib/ec2utils/ec2deprecateimg.py
@@ -22,127 +22,76 @@ import datetime
 import dateutil.relativedelta
 import re
 
-import ec2utils.ec2utilsutils as utils
-from ec2utils.ec2UtilsExceptions import *
+from .ec2utils import EC2Utils
+from .ec2UtilsExceptions import *
 
 
-class EC2DeprecateImg:
+class EC2DeprecateImg(EC2Utils):
     """Deprecate EC2 image(s) by tagging the image with 3 tags, Deprecated on,
        Removal date, and Replacement image."""
 
     def __init__(
             self,
-            account,
-            config_file_path=None,
+            access_key=None,
             deprecation_period=6,
             deprecation_image_id=None,
             deprecation_image_name=None,
             deprecation_image_name_fragment=None,
             deprecation_image_name_match=None,
             image_virt_type=None,
-            publi_only=None,
-            region=None,
+            public_only=None,
             replacement_image_id=None,
             replacement_image_name=None,
             replacement_image_name_fragment=None,
             replacement_image_name_match=None,
+            secret_key=None,
             verbose=None):
+        EC2Utils.__init__(self)
 
-        self.account = 'account-' + account
-        self.config_file_path = config_file_path
+        self.access_key = access_key
         self.deprecation_period = deprecation_period
-
-        self.config = utils.get_config(self.config_file_path)
-
-        self._verify_account()
-
-        self.access_key, self.secret_key = self._get_access_keys()
-
         self.deprecation_image_id = deprecation_image_id
         self.deprecation_image_name = deprecation_image_name
         self.deprecation_image_name_fragment = deprecation_image_name_fragment
         self.deprecation_image_name_match = deprecation_image_name_match
+        self.image_virt_type = image_virt_type
         self.public_only = public_only
-        self.region = region
-        self.replacement_imageID = replacement_imageID
-        self.replacement_imageName = replacement_imageName
-        self.replacement_imageNameFrag = replacement_imageNameFrag
-        self.replacement_imageNameMatch = replacement_imageNameMatch
+        self.replacement_image_id = replacement_image_id
+        self.replacement_image_name = replacement_image_name
+        self.replacement_image_name_fragment = replacement_image_name_fragment
+        self.replacement_image_name_match = replacement_image_name_match
+        self.secret_key = secret_key
         self.verbose = verbose
 
-        self.image_virt_type = None
-        if image_virt_type:
-            if image_virt_type[0:3] == 'par':
-                self.image_virt_type = 'paravirtual'
-            else:
-                self.image_virt_type = 'hvm'
-
         self._set_deprecation_date()
-        self._set_delete_date()
-        self.ec2 = None
-        if self.region:
-            self._connect()
-        self.replacement_image_id = None
+        self._set_deletion_date()
+
         self.replacement_image_tag = None
-        if self.ec2:
-            self._set_replacement_image_info()
-
-    # ---------------------------------------------------------------------
-    def _connect(self):
-        """Connect to EC2"""
-        if self.ec2:
-            return True
-
-        if self.region:
-            self.ec2 = boto.ec2.connect_to_region(
-                self.region,
-                aws_access_key_id=self.access_key,
-                aws_secret_access_key=self.secret_key
-            )
-        if not self.ec2:
-            msg = 'Could not connect to region: %s ' % self.region
-            raise EC2DeprecateImgException(msg)
-
-        if self.verbose:
-            print 'Connected to region: ', self.region
-
-        self._set_replacement_image_info()
-
-        return True
-
-    # ---------------------------------------------------------------------
-    def _disconnect(self):
-        """Disconnect from EC2"""
-        if self.ec2:
-            self.ec2.close()
-            self.ec2 = None
-            self.replacement_image_id = None
-            self.replacement_image_tag = None
 
     # ---------------------------------------------------------------------
     def _find_images_by_id(self, image_id, filter_replacement_image=None):
         """Find images by ID match"""
         images = []
-        images = self._get_all_type_match_images(filter_replacement_image)
-        for image in images:
-            if imgage_id == image.id:
+        my_images = self._get_all_type_match_images(filter_replacement_image)
+        for image in my_images:
+            if image_id == image.id:
                 images.append(image)
                 # The framework guarantees unique image IDs
                 return images
 
     # ---------------------------------------------------------------------
-    def _findImagesByName(self, img_name, filter_replacement_image=None):
+    def _find_images_by_name(self, image_name, filter_replacement_image=None):
         """Find images by exact name match"""
         images = []
         my_images = self._get_all_type_match_images(filter_replacement_image)
         for image in my_images:
             if not image.name:
-                if filterReplImg:
+                if filter_replacement_image:
                     msg = 'WARNING: Found image with no name, ignoring for '
                     msg += 'deprecation search. Image ID: %s' % image.id
                     print msg
                 continue
-            if imageName == image.name:
+            if image_name == image.name:
                 images.append(image)
 
         return images
@@ -175,7 +124,11 @@ class EC2DeprecateImg:
         """Find images by match the name with the given regular expression"""
         images = []
         my_images = self._get_all_type_match_images(filter_replacement_image)
-        match = re.compile(expression)
+        try:
+            match = re.compile(expression)
+        except:
+            msg = 'Unable to complie regular expression "%s"' % expression
+            raise EC2DeprecateImgException(msg)
         for image in my_images:
             if not image.name:
                 if filter_replacement_image:
@@ -204,27 +157,12 @@ class EC2DeprecateImg:
         return date
 
     # ---------------------------------------------------------------------
-    def _get_access_keys(self):
-        """Get the access keys for the account from the configuration"""
-        if not self.config.has_option(self.account, 'access_key_id'):
-            msg = 'No access_key_id option found for %s' % self.account
-            raise EC2DeprecateImgException(msg)
-        if not self.config.has_option(self.account, 'secret_access_key'):
-            msg = 'No secret_access_key option found for %s' % self.acount
-            raise EC2DeprecateImgException(msg)
-
-        accessKey = self.config.get(self.account, 'access_key_id')
-        secretKey = self.config.get(self.account, 'secret_access_key')
-
-        return accessKey, secretKey
-
-    # ---------------------------------------------------------------------
     def _get_all_type_match_images(self, filter_replacement_image=None):
         """Get all images that match thespecified virtualization type.
            All images owned by the account if not type is specified."""
         images = []
-        images = self.ec2.get_all_images(owners='self')
-        for image in images:
+        my_images = self.ec2.get_all_images(owners='self')
+        for image in my_images:
             if filter_replacement_image:
                 if image.id == self.replacement_image_id:
                     if self.verbose:
@@ -245,19 +183,17 @@ class EC2DeprecateImg:
                             images.append(image)
                             break
                 continue
-            images.append(img)
+            images.append(image)
 
         return images
 
     # ---------------------------------------------------------------------
     def _get_images_to_deprecate(self):
         """Find images to deprecate"""
-        if not self.has_connection():
-            self._connect()
         images = None
         condition = None
         if self.deprecation_image_id:
-            images = self._findImages_by_id(self.deprecation_image_id, True)
+            images = self._find_images_by_id(self.deprecation_image_id, True)
         elif self.deprecation_image_name:
             images = self._find_images_by_name(
                 self.deprecation_image_name, True)
@@ -279,7 +215,7 @@ class EC2DeprecateImg:
         """Set the date when the deprecation perios expires"""
         now = datetime.datetime.now()
         expire = now + dateutil.relativedelta.relativedelta(
-            months=+self.depPeriod)
+            months=+self.deprecation_period)
         self.deletion_date = self._format_date(expire)
 
     # ---------------------------------------------------------------------
@@ -291,8 +227,6 @@ class EC2DeprecateImg:
     # ---------------------------------------------------------------------
     def _set_replacement_image_info(self):
         """Find the replacement image information and create an identifier"""
-        if not self.has_connection():
-            self._connect()
         images = None
         condition = None
         if self.replacement_image_id:
@@ -327,18 +261,9 @@ class EC2DeprecateImg:
         self.replacement_image_tag = '%s -- %s' % (image.id, image.name)
 
     # ---------------------------------------------------------------------
-    def _verify_account(self):
-        """Verify that the specified account exists in the configuration"""
-        if not self.config.has_section(self.account):
-            msg = 'Could not find account specification for '
-            msg += self.account.split('account')[-1][1:]
-            msg += ' in config file '
-            msg += self.config_file_path
-        raise EC2DeprecateImgException(msg)
-
-    # ---------------------------------------------------------------------
-    def deprecate(self):
+    def deprecate_images(self):
         """Deprecate images in the connected region"""
+        self._connect()
         self._set_replacement_image_info()
         images = self._get_images_to_deprecate()
         if not images:
@@ -349,7 +274,7 @@ class EC2DeprecateImg:
             print 'Deprecating images in region: ', self.region
             print '\tDeprecated on', self.deprecation_date
             print 'Removal date', self.deletion_date
-            print 'Replacement image', self.replacementImageTag
+            print 'Replacement image', self.replacement_image_tag
         for image in images:
             if image.tags.get('Deprecated on', None):
                 if self.verbose:
@@ -362,16 +287,9 @@ class EC2DeprecateImg:
                 print '\t\ttagged:%s\t%s' % (image.id, image.name)
 
     # ---------------------------------------------------------------------
-    def has_connection(self):
-        """Return true if he object is connected to EC2"""
-        if self.ec2:
-            return True
-
-        return False
-
-    # ---------------------------------------------------------------------
     def print_deprecation_info(self):
         """Print information about the images that would be deprecated."""
+        self._connect()
         self._set_replacement_image_info()
         images = self._get_images_to_deprecate()
         if not images:
@@ -385,16 +303,5 @@ class EC2DeprecateImg:
         print '\tImages to deprecate:\n\t\tID\t\t\t\tName'
         for image in images:
             print '\t\t%s\t%s' % (image.id, image.name)
-
-        return True
-
-    # ---------------------------------------------------------------------
-    def set_region(self, region):
-        """Set the regionthat should be used."""
-        if self.region and self.region == region:
-            return True
-
-        self._disconnect()
-        self.region = region
 
         return True
--- ec2utils/ec2deprecateimg/python-ec2deprecateimg.spec
+++ ec2utils/ec2deprecateimg/python-ec2deprecateimg.spec
@@ -18,7 +18,7 @@
 
 %define upstream_name ec2deprecateimg
 Name:           python-ec2deprecateimg
-Version:        1.2.0
+Version:        2.0.0
 Release:        0
 Summary:        Tag image as deprected in EC2
 License:        GPL-3.0+
@@ -28,7 +28,7 @@ Source0:        %{upstream_name}-%{version}.tar.bz2
 Requires:       python
 Requires:       python-boto
 Requires:       python-dateutil
-Requires:       python-ec2utilsbase >= 0.1.0
+Requires:       python-ec2utilsbase >= 0.2.1
 BuildRequires:  python-setuptools
 BuildRoot:      %{_tmppath}/%{name}-%{version}-build
 
